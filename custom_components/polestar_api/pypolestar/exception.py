6980dd27bcd0bde909d3acb9fb26c4280336d164
Tuen Lee <tuen.lee@combell.group>
2023-12-23 20:01:58 +0100

    Add exception to catch failed calls

---
+++ custom_components/polestar_api/pypolestar/exception.py
@@ -0,0 +1,21 @@
+class PolestarApiException(Exception):
+    """Base class for exceptions in this module."""
+
+
+class PolestarAuthException(Exception):
+    """Base class for exceptions in Auth module."""
+    error_code: int = None
+    message: str = None
+
+    def __init__(self, message, error_code) -> None:
+        super().__init__(message)
+        self.error_code = error_code
+        return None
+
+
+class PolestarNotAuthorizedException(Exception):
+    """Exception for unauthorized call"""
+
+
+class PolestarNoDataException(Exception):
+    """Exception for no data"""

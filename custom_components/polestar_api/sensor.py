ccb17634cf584f99b0fa8d85046b9d268e93b9c1
Sebastian Loeb <mail.loeb@gmail.com>
2023-12-27 11:01:00 +0100

    Revert "Changed some sensor icons for better fit"
    This reverts commit 6d47ac24674db5ec82977f95d79962b83381f2fc.

--- custom_components/polestar_api/sensor.py
+++ custom_components/polestar_api/sensor.py
@@ -113,7 +113,7 @@ POLESTAR_SENSOR_TYPES: Final[tuple[PolestarSensorDescription, ...]] = (
     PolestarSensorDescription(
         key="average_speed_km_per_hour",
         name="Avg Speed Per Hour",
-        icon="mdi:speedometer",
+        icon="mdi:map-marker-distance",
         query="getOdometerData",
         field_name="averageSpeedKmPerHour",
         unit='km',
@@ -175,7 +175,7 @@ POLESTAR_SENSOR_TYPES: Final[tuple[PolestarSensorDescription, ...]] = (
     PolestarSensorDescription(
         key="charging_status",
         name="Charging status",
-        icon="mdi:ev-station",
+        icon="mdi:battery-clock",
         query="getBatteryData",
         field_name="chargingStatus",
         unit=None,
@@ -212,7 +212,7 @@ POLESTAR_SENSOR_TYPES: Final[tuple[PolestarSensorDescription, ...]] = (
     PolestarSensorDescription(
         key="charger_connection_status",
         name="Charging Connection Status",
-        icon="mdi:connection",
+        icon="mdi:battery-clock",
         query="getBatteryData",
         field_name="chargerConnectionStatus",
         unit=None,
@@ -286,7 +286,6 @@ POLESTAR_SENSOR_TYPES: Final[tuple[PolestarSensorDescription, ...]] = (
     PolestarSensorDescription(
         key="model_name",
         name="Model name",
-        icon="mdi:car-electric",
         query="getConsumerCarsV2",
         field_name="content/model/name",
         unit=None,
@@ -297,7 +296,6 @@ POLESTAR_SENSOR_TYPES: Final[tuple[PolestarSensorDescription, ...]] = (
     PolestarSensorDescription(
         key="last_updated_odometer_data",
         name="Last updated odometer data",
-        icon="mdi:counter",
         query="getOdometerData",
         field_name="eventUpdatedTimestamp/iso",
         unit=None,

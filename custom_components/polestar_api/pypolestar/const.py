de6977d83e5d8f76df7abd251c4c9c86d7268135
Tuen Lee <tuen.lee@combell.group>
2024-01-23 10:14:33 +0000

    add more status code for auth and api-v2

--- custom_components/polestar_api/pypolestar/const.py
+++ custom_components/polestar_api/pypolestar/const.py
@@ -6,3 +6,6 @@ ODO_METER_DATA = "getOdometerData"
 BATTERY_DATA = "getBatteryData"
 
 HTTPX_TIMEOUT = 30
+
+BASE_URL = "https://pc-api.polestar.com/eu-north-1/my-star/"
+BASE_URL_V2 = "https://pc-api.polestar.com/eu-north-1/mystar-v2/"

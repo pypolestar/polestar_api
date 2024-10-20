18a07e39f1cc6970456bceb68367d2001cdf3918
Jakob Schlyter <jakob@kirei.se>
2024-10-20 22:09:47 +0200

    move static URLs to const, use OIDC configuration to find endpoints when possible

--- custom_components/polestar_api/pypolestar/const.py
+++ custom_components/polestar_api/pypolestar/const.py
@@ -8,5 +8,11 @@ BATTERY_DATA = "getBatteryData"
 
 HTTPX_TIMEOUT = 30
 
+OPENID_PROVIDER_BASE_URL = "https://polestarid.eu.polestar.com"
+OPENID_REDIRECT_URI = "https://www.polestar.com/sign-in-callback"
+OPENID_CLIENT_ID = "polmystar"
+
+API_AUTH_URL = "https://pc-api.polestar.com/eu-north-1/auth/"
+
 BASE_URL = "https://pc-api.polestar.com/eu-north-1/my-star/"
 BASE_URL_V2 = "https://pc-api.polestar.com/eu-north-1/mystar-v2/"

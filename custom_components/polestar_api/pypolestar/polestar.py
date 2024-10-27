fd9fd768d2d85e3bcf46dbd4e1f641d238803bc7
Jakob Schlyter <jakob@kirei.se>
2024-10-27 12:06:41 +0100

    rename and reorder (gather all public methods)

--- custom_components/polestar_api/pypolestar/polestar.py
+++ custom_components/polestar_api/pypolestar/polestar.py
@@ -83,83 +83,6 @@ class PolestarApi:
             return self._get_field_name_value(field_name, data)
         return None
 
-    def _get_field_name_value(self, field_name: str, data: dict) -> str or bool or None:
-        if field_name is None or data is None:
-            return None
-
-        if "/" in field_name:
-            field_names = field_name.split("/")
-            for key in field_names:
-                if isinstance(data, dict) and key in data:
-                    data = data[key]
-                else:
-                    return None
-            return data
-
-        if isinstance(data, dict) and field_name in data:
-            return data[field_name]
-
-        return None
-
-    async def _get_odometer_data(self, vin: str):
-        """Get the latest odometer data from the Polestar API."""
-        params = {
-            "query": "query GetOdometerData($vin:String!){getOdometerData(vin:$vin){averageSpeedKmPerHour eventUpdatedTimestamp{iso unix}odometerMeters tripMeterAutomaticKm tripMeterManualKm}}",
-            "operationName": "GetOdometerData",
-            "variables": '{"vin":"' + vin + '"}',
-        }
-        result = await self.get_graph_ql(params, BASE_URL_V2)
-
-        if result and result["data"]:
-            # put result in cache
-            self.cache_data[ODO_METER_DATA] = {
-                "data": result["data"][ODO_METER_DATA],
-                "timestamp": datetime.now(),
-            }
-
-    async def _get_battery_data(self, vin: str):
-        params = {
-            "query": "query GetBatteryData($vin:String!){getBatteryData(vin:$vin){averageEnergyConsumptionKwhPer100Km batteryChargeLevelPercentage chargerConnectionStatus chargingCurrentAmps chargingPowerWatts chargingStatus estimatedChargingTimeMinutesToTargetDistance estimatedChargingTimeToFullMinutes estimatedDistanceToEmptyKm estimatedDistanceToEmptyMiles eventUpdatedTimestamp{iso unix}}}",
-            "operationName": "GetBatteryData",
-            "variables": '{"vin":"' + vin + '"}',
-        }
-
-        result = await self.get_graph_ql(params, BASE_URL_V2)
-
-        if result and result["data"]:
-            # put result in cache
-            self.cache_data[BATTERY_DATA] = {
-                "data": result["data"][BATTERY_DATA],
-                "timestamp": datetime.now(),
-            }
-
-    async def _get_vehicle_data(self):
-        """Get the latest vehicle data from the Polestar API."""
-        # get Vehicle Data
-        params = {
-            "query": "query GetConsumerCarsV2 { getConsumerCarsV2 { vin internalVehicleIdentifier salesType currentPlannedDeliveryDate market originalMarket pno34 modelYear registrationNo metaOrderNumber factoryCompleteDate registrationDate deliveryDate serviceHistory { claimType market mileage mileageUnit operations { id code description quantity performedDate } orderEndDate orderNumber orderStartDate parts { id code description quantity performedDate } statusDMS symptomCode vehicleAge workshopId } content { exterior { code name description excluded } exteriorDetails { code name description excluded } interior { code name description excluded } performancePackage { code name description excluded } performanceOptimizationSpecification { power { value unit } torqueMax { value unit } acceleration { value unit description } } wheels { code name description excluded } plusPackage { code name description excluded } pilotPackage { code name description excluded } motor { name description excluded } model { name code } images { studio { url angles resolutions } location { url angles resolutions } interior { url angles resolutions } } specification { battery bodyType brakes combustionEngine electricMotors performance suspension tireSizes torque totalHp totalKw trunkCapacity { label value } } dimensions { wheelbase { label value } groundClearanceWithPerformance { label value } groundClearanceWithoutPerformance { label value } dimensions { label value } } towbar { code name description excluded } } primaryDriver primaryDriverRegistrationTimestamp owners { id registeredAt information { polestarId ownerType } } wltpNedcData { wltpCO2Unit wltpElecEnergyConsumption wltpElecEnergyUnit wltpElecRange wltpElecRangeUnit wltpWeightedCombinedCO2 wltpWeightedCombinedFuelConsumption wltpWeightedCombinedFuelConsumptionUnit } energy { elecRange elecRangeUnit elecEnergyConsumption elecEnergyUnit weightedCombinedCO2 weightedCombinedCO2Unit weightedCombinedFuelConsumption weightedCombinedFuelConsumptionUnit } fuelType drivetrain numberOfDoors numberOfSeats motor { description code } maxTrailerWeight { value unit } curbWeight { value unit } hasPerformancePackage numberOfCylinders cylinderVolume cylinderVolumeUnit transmission numberOfGears structureWeek software { version versionTimestamp performanceOptimization { value description timestamp } } latestClaimStatus { mileage mileageUnit registeredDate vehicleAge } internalCar { origin registeredAt } edition commonStatusPoint { code timestamp description } brandStatus { code timestamp description } intermediateDestinationCode partnerDestinationCode features { type code name description excluded galleryImage { url alt } thumbnail { url alt } } electricalEngineNumbers { number placement } } }",
-            "operationName": "GetConsumerCarsV2",
-            "variables": '{"locale":"en_GB"}',
-        }
-
-        result = await self.get_graph_ql(params)
-        if result and result["data"]:
-            # check if there are cars in the account
-            if (
-                result["data"][CAR_INFO_DATA] is None
-                or len(result["data"][CAR_INFO_DATA]) == 0
-            ):
-                _LOGGER.exception("No cars found in account")
-                # throw new exception
-                raise PolestarNoDataException("No cars found in account")
-
-            return result["data"][CAR_INFO_DATA]
-            # self.cache_data[CAR_INFO_DATA] = {
-            #     "data": result["data"][CAR_INFO_DATA][0],
-            #     "timestamp": datetime.now(),
-            # }
-        return None
-
     async def get_ev_data(self, vin: str):
         """Get the latest ev data from the Polestar API."""
         if self.updating:
@@ -213,13 +136,90 @@ class PolestarApi:
                 return self._get_field_name_value(field_name, data)
         return None
 
+    def _get_field_name_value(self, field_name: str, data: dict) -> str or bool or None:
+        if field_name is None or data is None:
+            return None
+
+        if "/" in field_name:
+            field_names = field_name.split("/")
+            for key in field_names:
+                if isinstance(data, dict) and key in data:
+                    data = data[key]
+                else:
+                    return None
+            return data
+
+        if isinstance(data, dict) and field_name in data:
+            return data[field_name]
+
+        return None
+
+    async def _get_odometer_data(self, vin: str):
+        """Get the latest odometer data from the Polestar API."""
+        params = {
+            "query": "query GetOdometerData($vin:String!){getOdometerData(vin:$vin){averageSpeedKmPerHour eventUpdatedTimestamp{iso unix}odometerMeters tripMeterAutomaticKm tripMeterManualKm}}",
+            "operationName": "GetOdometerData",
+            "variables": '{"vin":"' + vin + '"}',
+        }
+        result = await self._get_graph_ql(params, BASE_URL_V2)
+
+        if result and result["data"]:
+            # put result in cache
+            self.cache_data[ODO_METER_DATA] = {
+                "data": result["data"][ODO_METER_DATA],
+                "timestamp": datetime.now(),
+            }
+
+    async def _get_battery_data(self, vin: str):
+        params = {
+            "query": "query GetBatteryData($vin:String!){getBatteryData(vin:$vin){averageEnergyConsumptionKwhPer100Km batteryChargeLevelPercentage chargerConnectionStatus chargingCurrentAmps chargingPowerWatts chargingStatus estimatedChargingTimeMinutesToTargetDistance estimatedChargingTimeToFullMinutes estimatedDistanceToEmptyKm estimatedDistanceToEmptyMiles eventUpdatedTimestamp{iso unix}}}",
+            "operationName": "GetBatteryData",
+            "variables": '{"vin":"' + vin + '"}',
+        }
+
+        result = await self._get_graph_ql(params, BASE_URL_V2)
+
+        if result and result["data"]:
+            # put result in cache
+            self.cache_data[BATTERY_DATA] = {
+                "data": result["data"][BATTERY_DATA],
+                "timestamp": datetime.now(),
+            }
+
+    async def _get_vehicle_data(self):
+        """Get the latest vehicle data from the Polestar API."""
+        # get Vehicle Data
+        params = {
+            "query": "query GetConsumerCarsV2 { getConsumerCarsV2 { vin internalVehicleIdentifier salesType currentPlannedDeliveryDate market originalMarket pno34 modelYear registrationNo metaOrderNumber factoryCompleteDate registrationDate deliveryDate serviceHistory { claimType market mileage mileageUnit operations { id code description quantity performedDate } orderEndDate orderNumber orderStartDate parts { id code description quantity performedDate } statusDMS symptomCode vehicleAge workshopId } content { exterior { code name description excluded } exteriorDetails { code name description excluded } interior { code name description excluded } performancePackage { code name description excluded } performanceOptimizationSpecification { power { value unit } torqueMax { value unit } acceleration { value unit description } } wheels { code name description excluded } plusPackage { code name description excluded } pilotPackage { code name description excluded } motor { name description excluded } model { name code } images { studio { url angles resolutions } location { url angles resolutions } interior { url angles resolutions } } specification { battery bodyType brakes combustionEngine electricMotors performance suspension tireSizes torque totalHp totalKw trunkCapacity { label value } } dimensions { wheelbase { label value } groundClearanceWithPerformance { label value } groundClearanceWithoutPerformance { label value } dimensions { label value } } towbar { code name description excluded } } primaryDriver primaryDriverRegistrationTimestamp owners { id registeredAt information { polestarId ownerType } } wltpNedcData { wltpCO2Unit wltpElecEnergyConsumption wltpElecEnergyUnit wltpElecRange wltpElecRangeUnit wltpWeightedCombinedCO2 wltpWeightedCombinedFuelConsumption wltpWeightedCombinedFuelConsumptionUnit } energy { elecRange elecRangeUnit elecEnergyConsumption elecEnergyUnit weightedCombinedCO2 weightedCombinedCO2Unit weightedCombinedFuelConsumption weightedCombinedFuelConsumptionUnit } fuelType drivetrain numberOfDoors numberOfSeats motor { description code } maxTrailerWeight { value unit } curbWeight { value unit } hasPerformancePackage numberOfCylinders cylinderVolume cylinderVolumeUnit transmission numberOfGears structureWeek software { version versionTimestamp performanceOptimization { value description timestamp } } latestClaimStatus { mileage mileageUnit registeredDate vehicleAge } internalCar { origin registeredAt } edition commonStatusPoint { code timestamp description } brandStatus { code timestamp description } intermediateDestinationCode partnerDestinationCode features { type code name description excluded galleryImage { url alt } thumbnail { url alt } } electricalEngineNumbers { number placement } } }",
+            "operationName": "GetConsumerCarsV2",
+            "variables": '{"locale":"en_GB"}',
+        }
+
+        result = await self._get_graph_ql(params)
+        if result and result["data"]:
+            # check if there are cars in the account
+            if (
+                result["data"][CAR_INFO_DATA] is None
+                or len(result["data"][CAR_INFO_DATA]) == 0
+            ):
+                _LOGGER.exception("No cars found in account")
+                # throw new exception
+                raise PolestarNoDataException("No cars found in account")
+
+            return result["data"][CAR_INFO_DATA]
+            # self.cache_data[CAR_INFO_DATA] = {
+            #     "data": result["data"][CAR_INFO_DATA][0],
+            #     "timestamp": datetime.now(),
+            # }
+        return None
+
     def _set_latest_call_code(self, url: str, code: int):
         if url == BASE_URL:
             self.latest_call_code = code
         else:
             self.latest_call_code_2 = code
 
-    async def get_graph_ql(self, params: dict, url: str = BASE_URL):
+    async def _get_graph_ql(self, params: dict, url: str = BASE_URL):
         """Get the latest data from the Polestar API."""
         headers = {
             "Content-Type": "application/json",
